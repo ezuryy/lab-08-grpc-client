71b52984495fd9dd56293104b1476011c5a3b79d
ezuryy <daria.kidinova@gmail.com>
2020-12-24 18:15:27 +0300

    initial commit

--- sources/main.cpp
+++ sources/main.cpp
@@ -9,25 +9,39 @@
 #include "echo.grpc.pb.h"
 #include "echo.pb.h"
 
+#include "suggestService.grpc.pb.h"
+#include "suggestService.pb.h"
+
 using grpc::ClientContext;
 using grpc::Status;
-using echo::HelloRequest;
-using echo::HelloResponse;
-using echo::Echo;
+using suggestService::SuggestRequest;
+using suggestService::Suggest;
+using suggestService::SuggestResponse;
+using suggestService::SuggestService;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
+    if (argc == 1)
+        throw std::invalid_argument("empty request");
 
-  auto channel = grpc::CreateChannel("0.0.0.0:9090", grpc::InsecureChannelCredentials());
-  auto client = Echo::NewStub(channel);
-  HelloResponse response;
-  ClientContext context;
-  HelloRequest request;
-  request.set_data("world");
-  Status status = client->Hello(&context, request, &response);
+    auto channel = grpc::CreateChannel("0.0.0.0:9090", grpc::InsecureChannelCredentials());
+    auto client = suggestService::NewStub(channel);
+    SuggestResponse response;
+    ClientContext context;
+    SuggestRequest request;
+    request.set_data(argv[1]);
+    Status status = client->Dialog(&context, request, &response);
 
-  if (status.ok()) {
-    std::cout << response.data() << std::endl;
-  } else {
-    return -1;
-  }
+    if (status.ok()) {
+        json result;
+        result["suggestions"] = {};
+        for (const auto &suggest : response.suggestions()) {
+            nlohmann::json element;
+            element["text"] = suggest.text();
+            element["position"] = suggest.position();
+            result["suggestions"].push_back(element);
+        }
+        std::cout << std::setw(2) << result << std::endl;
+    } else {
+        return -1;
+    }
 }
